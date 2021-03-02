66dda2e2280ebb86468e012eec581823f11c0614
Max Ivanov <waxivanov@gmail.com>
2021-03-02 16:49:05 +0200

    destructure data

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -5,29 +5,65 @@ type Props = {
   city: string;
 };
 
+// const convertToCelsius = (value: any) => {
+//   return value - 273.15;
+// };
+
 const WeatherData: React.FC<Props> = ({ city }) => {
-  const { data, loading } = useWeatherDataQuery({
+  const { data, loading, error } = useWeatherDataQuery({
     variables: { name: city },
   });
 
   if (loading) {
-    return <h1>Loading data...</h1>;
+    return <h1>Loading...</h1>;
   }
 
-  if (!data?.getCityByName) return <h1>Сity not found!</h1>;
+  if (error) {
+    return <h1>{`error: ${error?.message}`}</h1>;
+  }
+
+  if (!data) {
+    return <h1>Something went wrong.</h1>;
+  }
+
+  if (!data.getCityByName) {
+    return <h1>City not found.</h1>;
+  }
+
+  if (!data.getCityByName.weather) {
+    return <h1>Can't get temperature data.</h1>;
+  }
+
+  if (!data.getCityByName.weather.temperature) {
+    return <h1>Can't get temperature data.</h1>;
+  }
+
+  if (!data.getCityByName.weather.temperature.actual) {
+    return <h1>Can't get actual temperature data.</h1>;
+  }
+
+  if (!data.getCityByName.weather.summary) {
+    return <h1>Can't get summary data.</h1>;
+  }
+
+  const { name, country } = data.getCityByName;
+  const { actual } = data.getCityByName.weather.temperature;
+  const { description } = data.getCityByName.weather.summary;
+
+  // const celsius = useMemo(() => convertToCelsius(actual), [actual]);
 
   return (
     <div className='WeatherData'>
-      <h2>
-        {data.getCityByName.name}, {data.getCityByName.country}
-      </h2>
-
-      <h3>{data.getCityByName.weather?.summary?.description}</h3>
+      <h2>{`${name}: ${country}`}</h2>
 
       <h3>
-        Temperature:{' '}
-        {Math.round(Number(data.getCityByName.weather?.temperature?.actual) - 273.15)}
-        &deg;
+        <span>temperature: </span>
+        {`${actual}`}&deg;
+      </h3>
+
+      <h3>
+        <span>description: </span>
+        {`${description}`}
       </h3>
     </div>
   );
